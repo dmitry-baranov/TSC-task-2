307e0da749a9ad5b58fe6d8d680a671b72c6f28e
DBARANOV <dmitry-baranov>
2018-05-25 16:26:06 +0300

    commit - 1

---
+++ src/main/java/logic/Logic.java
@@ -0,0 +1,12 @@
+package logic;
+
+import uInteraction.data.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logic {
+    public void start() {
+        List<Data> listA = new ArrayList<Data>();
+    }
+}
---
+++ src/main/java/uInteraction/data/Data.java
@@ -0,0 +1,27 @@
+package uInteraction.data;
+
+public class Data {
+    public Data(int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    private int id;
+    private String value;
+}
