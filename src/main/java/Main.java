5a677b349d33c1d25626d799d934b1651ab01837
DBARANOV <dmitry-baranov>
2018-07-12 13:02:34 +0300

    last commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,11 @@ import logic.Logic;
 
 public class Main {
 
-    private static final String FILENAMEREADA = "textA.txt";
-
-    private static final String FILENAMEREADB = "textB.txt";
-
     private static Logic logic = new Logic();
 
     public static void main(String[] args) {
 
-        logic.start(FILENAMEREADA, FILENAMEREADB);
+        logic.start(args[0], args[1]);
 
     }
 }
